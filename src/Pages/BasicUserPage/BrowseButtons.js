c193f28c04e62cde7943300714e64113786cf87d
Eyob Bekele <eyobbekele99@gmail.com>
2023-06-27 08:58:20 +0300

    eyob to modify the css

--- src/Pages/BasicUserPage/BrowseButtons.js
+++ src/Pages/BasicUserPage/BrowseButtons.js
@@ -1,11 +1,46 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 import classes from "./BrowseButtons.module.css";
 
 function BrowseButtons() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/file/unapprallfiles")
+      .then((res) => {
+        return res;
+      })
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        setData(data);
+
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const clickHandler=()=>{
+    const win = window.open(data.file, "_blank");
+    win.focus();
+  }
+  
   return (
-    <div className={classes.group}>
-      <button>University</button>
-      <button>Personal Researcher</button>
+    <div className={classes.container}>
+      {data.map((uploaded) => {
+        const parts = uploaded.file.split("/");
+        const filenameWithExtension = parts[parts.length - 1];
+        const filename = filenameWithExtension.split(".")[0];
+
+        return (
+          <div className={classes.books} onClick={clickHandler} key={uploaded.id}>
+            <p>{uploaded.name}</p>
+            <p>{filename}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
